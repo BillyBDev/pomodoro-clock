fa4cb8343da094b63c7d641f5825f07c1e0c68e7
BillyBDev <BillyBDev@gmail.com>
2022-05-21 10:52:09 -0700

    built non-functioning skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+// what do we want to track in state?
+// current time
+// whether we're on break or session
+// break length and session length
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <div id="start_stop">
+        ▶ ⏸
+      </div>
+
+      <div id="reset">↺</div>
+      
+      <div id="middle">
+        <div id="break-label" class="top-mid-label">Break Length</div>
+        <div id="session-label" class="top-mid-label">Session Length</div>
+
+        <div id="break-decrement" class="adj-btn">ᐯ</div>
+        <div id="break-length">5</div>            
+        <div id="break-increment" class="adj-btn">ᐱ</div>
+
+        <div id="session-decrement" class="adj-btn">ᐯ</div>
+        <div id="session-length">25</div>                  
+        <div id="session-increment" class="adj-btn">ᐱ</div>
+      </div>
+
+      <div id="timer-label">Session</div>
+      
+      <div id="time-box">
+        <div id="time-left">0:00</div>
+      </div>
+
+
     </div>
   );
 }
